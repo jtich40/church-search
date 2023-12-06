72d2d9361ddbd002dd456b6ad3a00e0b3e129cd6
Jared Tichacek <jared.tichacek@gmail.com>
2023-12-06 17:42:21 -0600

    basic home page file structure

---
+++ app/home/page.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div>
+      <section>
+        <h1>Seek Sanctuary</h1>
+        <p>A personalized experience for finding your next church home.</p>
+        <h2>Start searching for your new church home now!</h2>
+        <input type="text" />
+      </section>
+      <section>
+        <h3>Step 1</h3>
+        <p>Search for churches in your area.</p>
+        <img src="https://picsum.photos/640/360" alt="" />
+      </section>
+      <section>
+        <h3>Step 2</h3>
+        <p>Find the church that best fits your needs.</p>
+        <img src="https://picsum.photos/640/360" alt="" />
+      </section>
+      <section>
+        <h3>Step 3</h3>
+        <p>Personalize your upcoming visit.</p>
+        <img src="https://picsum.photos/640/360" alt="" />
+      </section>
+      <section>
+        <h3>Step 4</h3>
+        <p>Start living out your faith in community.</p>
+        <img src="https://picsum.photos/640/360" alt="" />
+      </section>
+    </div>
+  );
+};
+
+export default Home;
